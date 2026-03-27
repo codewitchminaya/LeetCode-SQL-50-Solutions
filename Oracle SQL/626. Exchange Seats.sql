22fd626191087900aa610742a53f0c8f879b070c
codewitchminaya <minayabayramovaa@gmail.com>
2026-03-28 01:16:57 +0400

    third version

---
+++ Oracle SQL/626. Exchange Seats.sql
@@ -0,0 +1,62 @@
+ /*
+Table: Seat
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+ 
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+
+Explanation: 
+Note that if the number of students is odd, there is no need to change the last one's seat.
+*/
+WITH tabel AS (
+    SELECT id, 
+           student, 
+           LAG(student) OVER (ORDER BY id) AS student1,
+           LEAD(student) OVER (ORDER BY id) AS student2
+    FROM Seat
+) 
+SELECT id, 
+       CASE 
+           WHEN MOD(id,2) > 0 AND student2 IS NOT NULL THEN student2
+           WHEN MOD(id,2) = 0 THEN student1
+           ELSE student 
+       END AS student
+FROM tabel;
